d0cfb98c61f96d378be6d4e0ae6b125c2e066fac
Vincent20011128 <210410139@gms.tku.edu.tw>
2022-06-21 19:59:00 +0800

    S1-1 把老師w14週的模板index.html的title更改為animals API

--- final_210410139/cocktaildb_xx/app_xx.js
+++
@@ -1,8 +0,0 @@
-import presentIngredient from './src/presentDrinks_xx.js';
-import './src/searchForm_xx.js';
-
-const URL = 'https://zoo-animal-api.herokuapp.com/animals/rand/10';
-
-window.addEventListener('DOMContentLoaded', () => {
-  presentIngredient(URL);
-});
