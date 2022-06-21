7606b0d22e9ee1db5cf8714f024d427e1a4971be
Vincent20011128 <210410139@gms.tku.edu.tw>
2022-06-21 21:36:58 +0800

    S2-1 加入app.js檔讓index.html讀取到

--- Project_210410139/app_xx.js
+++
@@ -1,8 +0,0 @@
-import presentAnimals from "./src/presentAnimals_xx.js";
-import "./src/searchForm_xx.js";
-
-const URL = "https://zoo-animal-api.herokuapp.com/animals/rand/10";
-
-window.addEventListener("DOMContentLoaded", () => {
-  presentAnimals(URL);
-});
